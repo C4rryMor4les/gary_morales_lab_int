5cccc76d797a8f7c83ee51f0a0354be27ea97d17
garymorales <garyamt@gmail.com>
2023-05-25 23:50:39 -0500

    add file of requirements.txt

--- owner/admin.py
+++ owner/admin.py
@@ -3,4 +3,10 @@ from owner.models import Owner
 
 
 # Register your models here.
-admin.site.register(Owner)
+#admin.site.register(Owner)
+@admin.register(Owner)
+class OwnerAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'pais', 'vigente')
+    list_filter = ('pais','nombre')
+    search_fields = ('nombre','pais')
+    fields = ('nombre','pais')
--- owner/views.py
+++ owner/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+def owner_list(request):
+    data_context = {
+        'nombre' : 'Katty Paredes',
+        'edad' : 24,
+        'dni' : 12345678,
+        'pais' : 'Peru'
+    }
+
+    return render(request,'owner/owner_list.html',data_context)
