a87a44a46f1fd796dab7361c12221c0cbb478f90
garymorales <garyamt@gmail.com>
2023-05-30 13:10:13 -0500

    add file gitignore

--- owner/views.py
+++ owner/views.py
@@ -2,11 +2,65 @@ from django.shortcuts import render
 
 # Create your views here.
 def owner_list(request):
-    data_context = {
-        'nombre' : 'Katty Paredes',
-        'edad' : 24,
-        'dni' : 12345678,
-        'pais' : 'Peru'
-    }
-
-    return render(request,'owner/owner_list.html',data_context)
+    # data_context ={
+    #         'nombre': 'Katty Paredes',
+    #         'edad': 16,
+    #         'dni': 42425656,
+    #         'pais': 'Peru',
+    #         'vigente': False
+    # },
+    data_context = [
+        {
+            'nombre': 'Katty Paredes',
+            'edad' : 16,
+            'dni' : 42425656,
+            'pais' : 'Peru',
+            'vigente' : False,
+            'pokemons':[
+                {
+                    'nombre_pokemon': 'Charizard',
+                    'ataques':['Ataque 1 - Charizard', 'Ataque 2 - Charizard', 'Ataque 3 - Charizard' ]
+                }
+            ]
+        },
+        {
+            'nombre': 'Miguel Valera',
+            'edad': 26,
+            'dni': 43454545,
+            'pais': 'España',
+            'vigente': False,
+            'pokemons': [
+                {
+                    'nombre_pokemon': 'Charizard',
+                    'ataques': ['Ataque 1 - Charizard', 'Ataque 2 - Charizard', 'Ataque 3 - Charizard']
+                }
+            ]
+        },
+        {
+            'nombre': 'Jesus de la Cruz',
+            'edad': 30,
+            'dni': 44444545,
+            'pais': 'Colombia',
+            'vigente': False,
+            'pokemons': [
+                {
+                    'nombre_pokemon': 'Charizard',
+                    'ataques': ['Ataque 1 - Charizard', 'Ataque 2 - Charizard', 'Ataque 3 - Charizard']
+                }
+            ]
+        },
+        {
+            'nombre': 'Liliana Vargas',
+            'edad': 24,
+            'dni': 454546436,
+            'pais': 'España',
+            'vigente': True,
+            'pokemons': [
+                {
+                    'nombre_pokemon': 'Charizard',
+                    'ataques': ['Ataque 1 - Charizard', 'Ataque 2 - Charizard', 'Ataque 3 - Charizard']
+                }
+            ]
+        }
+    ]
+    return render(request,'owner/owner_list.html',context={'data':data_context})
